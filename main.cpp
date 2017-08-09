821b5595b82ce38a78b978fa818b0a1fbe816698
mahmoud <mahmoudhas@outlook.com>
2017-08-09 17:23:01 -0400

    ..

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 #include "Display.h"
 #include "Shader.h"
+#include "Mesh.h"
 
 int main() {
     Display display(800, 600, "hello world");
     Shader shader("./res/shader.vert", "./res/shader.frag");
 
+
+    Vertex vertices[] = {
+            Vertex(glm::vec3(-0.5, -0.5, 0)),
+            Vertex(glm::vec3(0, 0.5, 0)),
+            Vertex(glm::vec3(0.5, -0.5, 0))
+    };
+
+
+    Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+
     while (!display.isClosed()) {
         display.Clear(0, 0, 0, 1);
 
         shader.Bind();
+        mesh.Draw();
 
 
         display.Update();
